8bc0b9356e599997beda8f9a95c744881e685fa4
MR.Rezoo <rezam578@gmail.com>
2020-12-24 17:04:25 +0330

    User Module => set seeder | build model and relations

--- Modules/User/Database/Seeders/UserDatabaseSeeder.php
+++ Modules/User/Database/Seeders/UserDatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Modules\User\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+use Modules\User\Entities\User;
 
 class UserDatabaseSeeder extends Seeder
 {
@@ -15,7 +17,25 @@ class UserDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-
+        $this->create_user();
         // $this->call("OthersTableSeeder");
+    }
+
+
+    private function create_user()
+    {
+
+        $users = array(
+            [
+                'name' => 'Admin',
+                'mobile' => '09335668353',
+                'password' => 'AdminAdmin',
+            ],
+
+        );
+        foreach ($users as $user) {
+            User::updateOrCreate($user);
+        }
+
     }
 }
--- Modules/User/Entities/User.php
+++ Modules/User/Entities/User.php
@@ -4,15 +4,42 @@ namespace Modules\User\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+use Modules\Contact\Entities\Ticket;
 
 class User extends Model
 {
-    use HasFactory;
 
-    protected $fillable = [];
-    
-    protected static function newFactory()
+    protected $fillable = ['name', 'mobile', 'password'];
+
+    protected $table = 'users';
+
+    /**
+     * ATTENTION : bcrypt password that you enter on field
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
     {
-        return \Modules\User\Database\factories\UserFactory::new();
+        $this->attributes['password'] = bcrypt($value);
     }
+
+    /**
+     * ATTENTION : if you dont set password this method creating random password during user creating
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::creating(function (User $user) {
+            if (!$user->password)
+                $user->password = Str::random(8);
+        });
+    }
+
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
 }
